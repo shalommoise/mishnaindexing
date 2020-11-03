f63e287422b4a0059945745cde60df5753374ca5
Shalommoise <mshalom689@gmail.com>
2020-11-03 12:49:02 +0000

    getNUmbers gets index of 4

--- numbers.js
+++ numbers.js
@@ -1,3 +1,15 @@
-const getNumbers = ()=>{
+exports.getNumbers = (start, end)=>{
+  let mishnayos = []
+if(!start && !end) return mishnayos;
+const funStart = start*10;
+const funEnd =end*10;
+mishnayos = [funStart, funStart +1, funEnd -1, funEnd];
+if( funEnd.toString().endsWith('3')){
+  mishnayos = [funStart, funEnd -2, funEnd -1, funEnd];
+}
+if( funEnd.toString().endsWith('1')){
+  mishnayos = [funStart, funStart +1, funStart +2, funEnd];
+}
 
+return mishnayos.map((mishna)=> mishna/10)
 }
--- numbers.test.js
+++ numbers.test.js
@@ -4,4 +4,21 @@ describe('getNumbers', ()=>{
   test('returns empty array ', () => {
    expect(getNumbers()).toEqual([]) 
   });
+  test('returns, [1.1,1.2,1.3,1.4] ', () => {
+       expect(getNumbers(1.1,1.4)).toEqual([1.1,1.2,1.3,1.4]) 
+  });
+  test('works for end of chapter', () => {
+       expect(getNumbers(1.5,2.2)).toEqual([1.5,1.6,2.1,2.2]) 
+  });
+test('works for last mishna of 1', ()=>{
+  expect(getNumbers(1.5,2.3)).toEqual([1.5,2.1,2.2, 2.3])
+})
+test('ends with new Chapter', ()=>{
+  expect(getNumbers(1.5,2.1)).toEqual([1.5,1.6,1.7, 2.1])
+})
+test('random tests', ()=>{
+  expect(getNumbers(1.5,2.1)).toEqual([1.5,1.6,1.7, 2.1]);
+   expect(getNumbers(1.6,2.1)).toEqual([1.6,1.7,1.8, 2.1]);
+   expect(getNumbers(30.5,30.8)).toEqual([30.5,30.6,30.7,30.8]);
+})
 })
